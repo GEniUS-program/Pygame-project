1877ea55fbbc85be7f8b77faa1e68a9aa1d2b76c
Роман <romochka.nikitin.16@mail.ru>
2025-02-02 18:50:17 +0500

    Another small tweak

--- pygame_file.py
+++ pygame_file.py
@@ -12,7 +12,7 @@ pygame.mixer.init()
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Simple Shooter Game")
+pygame.display.set_caption("Space Invaders: Galactic Defense")
 
 # Set the frame rate
 clock = pygame.time.Clock()
